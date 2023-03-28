390f061c599b5e27bd952f96b92bca27e55d2f89
bugulov <121980063+bugulov@users.noreply.github.com>
2023-03-28 09:49:34 +0300

    Create robot.h

---
+++ Source/robot.h
@@ -0,0 +1,18 @@
+#pragma once
+#include "Position.h"
+
+class Robot {
+public:
+  void moveForward(){
+    //doing smth...
+  };
+  void setAngle(){
+    //doing smth...
+  };
+  void moveBack(){
+    //doing smth...
+  };
+  void stop(){
+    //doing smth...
+  };
+};
