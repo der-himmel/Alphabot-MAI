1e31169bd9f16f591fda7fa3db171bfafb88bb4d
der-himmel <105467009+der-himmel@users.noreply.github.com>
2023-05-16 07:49:07 +0300

    Update and rename FSM.cpp to Control.cpp

---
+++ Source/Control.cpp
@@ -0,0 +1,74 @@
+#include "opencv2/opencv.hpp"
+#include <iostream>
+#include <Windows.h>
+#include "Control.h"
+#include "Robot.h"
+#include "Cam.h"
+
+Control::Control(Cam* cam, Robot* bot)
+{
+	this->flag = 1;
+	this->status = 0;
+	this->cam = cam;
+	this->bot = bot;
+}
+
+void Control::operating() 
+{
+	while(1)
+	{
+		cam->update();
+		operating();
+	}
+}
+
+void Control::moving()
+{
+	switch (status)
+	{
+	case 0:
+	{
+		while (this->flag)
+		{
+			if (GetKeyState(VK_RSHIFT) & 0x8000)
+			{
+				std::cout << "Order received!";
+				Sleep(1000);
+				system("cls");
+				flag = 0;
+			}
+			else
+			{
+				bot->waitingMode();
+			}
+		}
+	}
+	case 1:
+		if (this->cam->angle > 10)
+		{
+			status = 3;
+		}
+		else
+		{
+			status = 2;
+		}
+
+		if (cam->dist >= 400)
+		{
+			bot->moveForward();
+		}
+		else
+		{
+			status = 0;
+			std::cout << "Order delivered.";
+		}
+
+	case 2:
+		bot->rotateLeft();
+		break;
+
+	case 3:
+		bot->rotateRight();
+		break;
+	}
+}
