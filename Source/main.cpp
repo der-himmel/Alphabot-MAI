1f8a16a6a052a695fbd20a18a4e7b1202dcec267
der-himmel <105467009+der-himmel@users.noreply.github.com>
2023-05-16 07:50:27 +0300

    Update main.cpp

--- Source/main.cpp
+++ Source/main.cpp
@@ -1,36 +1,22 @@
 #include <iostream>
-#include <opencv2>
-#include "Robot.h"
-#include "RobotContr.h"
+#include <string>
 #include "Cam.h"
+#include "Control.h"
+#include "Robot.h"
 
-class Server{
-public:
-    void getImage(); //gets and processes image from camera
-    void handleOrder(); //receives order
-    void routeBuild(); //builds route according to camera output
-    void robotControl(); //makes the robot move
-};
+#define mqtt_host "192.168.1.4"
+#define mqtt_port 1883
 
-class Dispencer{
-private:
-    float x, y; //constant coordinates of the dispencer
-public:
-    void dHandleCommunication(); //handles communication with the server
-};
+using namespace std;
+using namespace cv;
 
-class Customer{
-private:
-    float x, y; //coordinates
-public:
-    void requestOrder(); //makes an order request to the server
-    void receiveOrder(); //receives order from robot
-};
 
-int main() {
-    Cam *cam = new Cam(0);
-    Robot *robot = new Robot();
-    RobotContr *rcontr = new RobotContr(cam, robot);
-    std::cout << "Not working yet..." << std::endl;
-    return 0;
+int main()
+{
+    Robot* bot = new Robot(mqtt_host, mqtt_port);
+    Cam* cam = new Cam(0);
+
+    Control* control = new Control(cam, bot);
+
+    control->operating();
 }
