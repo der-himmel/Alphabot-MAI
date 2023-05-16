24342f10fdf605128ff8e28c07a260fd490edba4
der-himmel <105467009+der-himmel@users.noreply.github.com>
2023-05-16 07:46:33 +0300

    Update Cam.cpp

--- Source/Cam.cpp
+++ Source/Cam.cpp
@@ -1,4 +1,3 @@
-#pragma ide diagnostic ignored "EndlessLoop"
 #include "opencv2/opencv.hpp"
 #include <iostream>
 #include <string>
@@ -9,120 +8,121 @@ using namespace cv;
 using namespace std;
 
 
-Cam::Cam(int i) {
+Cam::Cam(int i) 
+{
     this->cap.open(i);
 
-    if(!this->cap.isOpened()){
-        cout << "Error. Cannot open the video source." << endl;
+    if (!this->cap.isOpened()) 
+    {
+        cout << "Error. Can't open the video source." << endl;
     }
 }
 
-Mat Cam::getFrame() {
+Mat Cam::getFrame() 
+{
     cap.read(img);
     return img;
 }
 
-void Cam::updateFrame() {
+void Cam::update() 
+{
     img = getFrame();
-    img = findRobot(img);
-    midREQ = findReqMid(img);
+    
+    res = img.clone();
+    res = findRobot(res);
+
+    midREQ = findReqMid(res, minReqColor, maxReqColor);
     midROBOT = calcRobotMid(frontR, backR);
 
-    calcDist(midROBOT, midREQ);
-    calcAngle(frontR, backR, midROBOT, midREQ);
+    dist = calcDist(midROBOT, midREQ);
+    angle = calcAngle(frontR, backR, midROBOT, midREQ);
+
+    cout << dist << "; " << angle;
+    system("cls");
+
+    imshow("Image", img);
+    imshow("Output", res);
 }
 
-Mat Cam::findRobot(Mat img) {
-    cvtColor(img, imgHSV, COLOR_BGR2HSV);
+Mat Cam::findRobot(Mat res) 
+{
+    cvtColor(res, imgHSV, COLOR_BGR2HSV);
 
     inRange(imgHSV, Scalar(162, 137, 137), Scalar(172, 255, 255), maskRED);
     inRange(imgHSV, Scalar(68, 137, 137), Scalar(122, 255, 255), maskBLUE);
 
     Moments mFront = moments(maskRED, true);
-    frontR.x = mFront.m10/mFront.m00;
-    frontR.y = mFront.m01/mFront.m00;
-    for (int y = 0; y < maskRED.rows; y++) {
-        for (int x = 0; x < maskRED.cols; x++) {
+    frontR.x = mFront.m10 / mFront.m00;
+    frontR.y = mFront.m01 / mFront.m00;
+    for (int y = 0; y < maskRED.rows; y++) 
+    {
+        for (int x = 0; x < maskRED.cols; x++) 
+        {
             int value = maskRED.at<uchar>(y, x);
-            if (value == 255) {
+            if (value == 255) 
+            {
                 int count = floodFill(maskRED, Point(x, y), Scalar(200), &rectFRONT);
-                if (rectFRONT.width >= 10 && rectFRONT.height >= 10) {
-                    rectangle(img, rectFRONT, Scalar(255, 0, 255, 4), 2);
+                if (rectFRONT.width >= 10 && rectFRONT.height >= 10) 
+                {
+                    rectangle(res, rectFRONT, Scalar(255, 0, 255, 4), 2);
                 }
             }
         }
     }
-    circle(img, frontR, 5, Scalar(255, 255, 255), -1);
-    putText(img, "FRONT", Point(frontR.x + 10, frontR.y + 20), FONT_HERSHEY_DUPLEX, 0.8, CV_RGB(255, 255, 255), 2);
+    circle(res, frontR, 5, Scalar(255, 255, 255), -1);
+    putText(res, "FRONT", Point(frontR.x + 10, frontR.y + 20), FONT_HERSHEY_DUPLEX, 0.8, CV_RGB(255, 255, 255), 2);
 
 
-    Moments mBack= moments(maskBLUE, true);
-    backR.x = mBack.m10/mBack.m00;
-    backR.y = mBack.m01/mBack.m00;
-    for (int y = 0; y < maskBLUE.rows; y++) {
-        for (int x = 0; x < maskBLUE.cols; x++) {
+    Moments mBack = moments(maskBLUE, true);
+    backR.x = mBack.m10 / mBack.m00;
+    backR.y = mBack.m01 / mBack.m00;
+    for (int y = 0; y < maskBLUE.rows; y++) 
+    {
+        for (int x = 0; x < maskBLUE.cols; x++) 
+        {
             int value = maskBLUE.at<uchar>(y, x);
-            if (value == 255) {
+            if (value == 255) 
+            {
                 int count = floodFill(maskBLUE, Point(x, y), Scalar(200), &rectBACK);
-                if (rectBACK.width >= 10 && rectBACK.height >= 10) {
-                    rectangle(img, rectBACK, Scalar(255, 0, 255, 4), 2);
+                if (rectBACK.width >= 10 && rectBACK.height >= 10) 
+                {
+                    rectangle(res, rectBACK, Scalar(255, 0, 255, 4), 2);
                 }
             }
         }
     }
-    circle(img, backR, 5, Scalar(255, 255, 255), -1);
-    putText(img, "BACK", Point(backR.x + 10, backR.y - 20), FONT_HERSHEY_DUPLEX, 0.8, CV_RGB(255, 255, 255), 2);
+    circle(res, backR, 5, Scalar(255, 255, 255), -1);
+    putText(res, "BACK", Point(backR.x + 10, backR.y - 20), FONT_HERSHEY_DUPLEX, 0.8, CV_RGB(255, 255, 255), 2);
 
-    imshow("Image", img);
-    return img;
+    return res;
 }
 
-Point2i Cam::findReqMid(Mat frame) {
-    Moments mReq= moments(maskREQUEST, true);
-    midREQ.x = mReq.m10/mReq.m00;
-    midREQ.y = mReq.m01/mReq.m00;
-    for (int y = 0; y < maskREQUEST.rows; y++) {
-        for (int x = 0; x < maskREQUEST.cols; x++) {
-            int value = maskREQUEST.at<uchar>(y, x);
-            if (value == 255) {
-                int count = floodFill(maskREQUEST, Point(x, y), Scalar(200), &rectREQ);
-                if (rectREQ.width >= 10 && rectREQ.height >= 10) {
-                    rectangle(img, rectREQ, Scalar(255, 0, 255, 4), 2);
-                }
-            }
-        }
-    }
-    circle(img, midREQ, 5, Scalar(255, 255, 255), -1);
-    putText(img, "REQUESTER", Point(midREQ.x - 60, midREQ.y + 100), FONT_HERSHEY_DUPLEX, 0.8, CV_RGB(255, 255, 255), 2);
+Point2i Cam::findReqMid(Mat res, Scalar &minReqColor, Scalar& maxReqColor)
+{
+    inRange(imgHSV, minReqColor, maxReqColor, maskREQUEST);
 
-    imshow("Image", img);
+    Moments mReq = moments(maskREQUEST, true);
+    midREQ.x = mReq.m10 / mReq.m00;
+    midREQ.y = mReq.m01 / mReq.m00;
+
     return midREQ;
 }
 
-Point2i Cam::calcRobotMid(Point2i frontR, Point2i backR){
-    midROBOT = (frontR + backR)/2;
-    circle(img, midROBOT, 5, Scalar(255, 255, 255), -1);
-    putText(img, "MIDDLE", Point(midROBOT.x + 10, midROBOT.y), FONT_HERSHEY_DUPLEX, 0.8, CV_RGB(255, 255, 255), 2);
-
-    imshow("Image", img);
+Point2i Cam::calcRobotMid(Point2i frontR, Point2i backR) 
+{
+    midROBOT = (frontR + backR) / 2;
     return midROBOT;
 }
 
-int Cam::calcDist(Point2i midRobot, Point2i midReq) {
-    int distance = sqrt((midREQ.x - midROBOT.x)*(midREQ.x - midROBOT.x) + (midREQ.y - midROBOT.y)*(midREQ.y - midROBOT.y));
-    putText(img, "DISTANCE: " + to_string(distance), Point(img.cols - 220, img.rows - 45),FONT_HERSHEY_DUPLEX, 0.7, CV_RGB(255, 255, 255), 2);
-
-    imshow("Image", img);
+int Cam::calcDist(Point2i midRobot, Point2i midReq) 
+{
+    int distance = sqrt((midREQ.x - midROBOT.x) * (midREQ.x - midROBOT.x) + (midREQ.y - midROBOT.y) * (midREQ.y - midROBOT.y));
     return distance;
 }
 
-int Cam::calcAngle(Point2i frontR, Point2i backR, Point2i midRobot, Point2i midReq) {
-    double cos = ((frontR.x - backR.x)*(midREQ.x - midROBOT.x) + (frontR.y - backR.y)*(midREQ.y - midROBOT.y)) / (sqrt((frontR.x - backR.x)*(frontR.x - backR.x) + (frontR.y - backR.y)*(frontR.y - backR.y)) * sqrt((midREQ.x - midROBOT.x)*(midREQ.x - midROBOT.x) + (midREQ.y - midROBOT.y)*(midREQ.y - midROBOT.y)));
-    cout << cos << " ------ ";
-    cout << midREQ.y << ' ' << midROBOT.y << "; " << endl;
-    double angle = acos(cos)*180/3.14;
-    putText(img, "ANGLE: " + to_string(angle), Point(img.cols - 220, img.rows - 20),FONT_HERSHEY_DUPLEX, 0.7, CV_RGB(255, 255, 255), 2);
-
-    imshow("Image", img);
+double Cam::calcAngle(Point2i frontR, Point2i backR, Point2i midRobot, Point2i midReq) 
+{
+    double cos = ((frontR.x - backR.x) * (midREQ.x - midROBOT.x) + (frontR.y - backR.y) * (midREQ.y - midROBOT.y)) / (sqrt((frontR.x - backR.x) * (frontR.x - backR.x) + (frontR.y - backR.y) * (frontR.y - backR.y)) * sqrt((midREQ.x - midROBOT.x) * (midREQ.x - midROBOT.x) + (midREQ.y - midROBOT.y) * (midREQ.y - midROBOT.y)));
+    double angle = acos(cos) * 180 / 3.14;
     return angle;
 }
