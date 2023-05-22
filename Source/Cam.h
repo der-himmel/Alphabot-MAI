eb49bfdeabe60d32970c4f25eb513f80f293aec1
der-himmel <105467009+der-himmel@users.noreply.github.com>
2023-05-22 20:13:54 +0300

    Update Cam.h

--- Source/Cam.h
+++ Source/Cam.h
@@ -8,19 +8,22 @@ using namespace cv;
 class Cam 
 {
 private:
-    Mat img, res;
+    Mat img, result;
     Mat imgHSV, maskRED, maskBLUE, maskREQUEST, maskROBOT;
 
     Point2i frontR, backR, midREQ, midROBOT;
     Rect rectFRONT, rectBACK, rectREQ;
 
-    Scalar minReqColor(int hminREQ, int sminREQ, int vminREQ);
-    Scalar maxReqColor(int hmaxREQ, int smaxREQ, int vmaxREQ);
+    //Scalar minReqColor(int hminREQ, int sminREQ, int vminREQ);
+    //Scalar maxReqColor(int hmaxREQ, int smaxREQ, int vmaxREQ);
+
+    Scalar minReqColor = Scalar(30, 125, 100);
+    Scalar maxReqColor = Scalar(60, 255, 255);
 
     VideoCapture cap;
     Mat getFrame();
     Mat findRobot(Mat frame);
-    Point2i findReqMid(Mat frame, Scalar& minReqColor, Scalar& maxReqColor);
+    Mat findReqMid(Mat frame, Scalar& minReqColor, Scalar& maxReqColor);
     Point2i calcRobotMid(Point2i frontR, Point2i backR);
 
 public:
