4efa24cd495b59038dc2b262dc3bac4c735b00e6
der-himmel <105467009+der-himmel@users.noreply.github.com>
2023-05-22 20:14:34 +0300

    Update Control.cpp

--- Source/Control.cpp
+++ Source/Control.cpp
@@ -11,13 +11,12 @@ Control::Control(Cam* cam, Robot* bot)
 	this->status = 0;
 	this->cam = cam;
 	this->bot = bot;
-}
+};
 
 void Control::operating() 
 {
 	while(1)
 	{
-		cam->update();
 		moving();
 	}
 }
@@ -30,45 +29,71 @@ void Control::moving()
 	{
 		while (this->flag)
 		{
-			if (GetKeyState(VK_RSHIFT) & 0x8000)
+			if (GetKeyState(VK_RSHIFT) & 0x08000)
 			{
-				std::cout << "Order received!";
+				this->flag = 0;
+				std::cout << "Order received!" << std::endl;
 				Sleep(1000);
-				system("cls");
-				flag = 0;
+				this->status = 1;
 			}
 			else
 			{
 				bot->waitingMode();
+				cam->update();
+				std::cout << "Waiting..." << std::endl;
+				Sleep(100);
 			}
 		}
 	}
 	case 1:
-		if (this->cam->angle > 10)
+		cam->update();
+		
+		if (cam->dist >= 150 & abs(cam->angle) < 10)
 		{
-			status = 3;
+			std::cout << cam->dist << ' ';
+			bot->moveForward();
+			std::cout << "Forward..." << std::endl;
+			break;
 		}
-		else
+		else if (cam->dist < 150)
+		{
+			std::cout << "Order delivered!" << std::endl;
+			Sleep(1000);
+			this->flag = 1;
+			status = 0;
+			break;
+		}
+		else 
 		{
 			status = 2;
+			break;
 		}
-
-		if (cam->dist >= 400)
-		{
-			bot->moveForward();
-		}
-		else
-		{
-			status = 0;
-			std::cout << "Order delivered.";
-		}
-
 	case 2:
-		bot->rotateLeft();
-		break;
+		cam->update();
+
+		if (cam->angle > 0)
+		{
+			std::cout << "angle = " << cam->angle << ' ';
+			status = 4;
+			break;
+		}
+		else if (cam->angle < 0 & cam->angle >= -30)
+		{
+			std::cout << "angle = " << cam->angle << ' ';
+			status = 3;
+			break;
+		}
 
 	case 3:
+		bot->rotateLeft();
+		std::cout << "Left..." << std::endl;
+		status = 1;
+		break;
+
+	case 4:
 		bot->rotateRight();
+		std::cout << "Right..." << std::endl;
+		status = 1;
 		break;
 	}
 }
