c376514aa9d2ad84e9beab30dc8d0fad28798171
der-himmel <105467009+der-himmel@users.noreply.github.com>
2023-05-22 20:16:58 +0300

    Update main.cpp

--- Source/main.cpp
+++ Source/main.cpp
@@ -1,5 +1,6 @@
 #include <iostream>
 #include <string>
+#include <Windows.h>
 #include "Cam.h"
 #include "Control.h"
 #include "Robot.h"
@@ -13,8 +14,12 @@ using namespace cv;
 
 int main()
 {
-    Robot* bot = new Robot(mqtt_host, mqtt_port);
+    // Robot* bot = new Robot(mqtt_host, mqtt_port);
+    Robot* bot = new Robot();
     Cam* cam = new Cam(0);
+
+    cout << "Robot and Camera instances successfully created" << endl;
+    Sleep(1000);
 
     Control* control = new Control(cam, bot);
 
